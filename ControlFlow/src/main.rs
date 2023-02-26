9299e77e627b7495387bba63f8377b1a99bec827
Tom Victor <vjtomvictor@icloud.com>
2023-02-26 19:45:33 +0100

    rust control flow

---
+++ ControlFlow/src/main.rs
@@ -0,0 +1,101 @@
+fn main() {
+    // if
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    let number = 3;
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+
+    // if else
+
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    // Using if in a let Statement
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+
+    // loop
+
+    // loop {
+    //     println!("again!");
+    // }
+
+    // Returning Values from Loops
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+
+    // Loop Labels to Disambiguate Between Multiple Loops
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    // Conditional Loops with while
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    // Looping Through a Collection with for
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    for number in (1..9).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
