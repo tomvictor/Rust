d4d75dc1a387fcbd52ffbf9ee97ea3b938c3a097
Tom Victor <vjtomvictor@icloud.com>
2023-03-03 18:40:22 +0100

    update run fn with rumqttc

--- Python Rust Binding/src/run.rs
+++ Python Rust Binding/src/run.rs
@@ -1,19 +1,20 @@
+use rumqttc::{MqttOptions, Client, QoS};
+use std::time::Duration;
 use std::thread;
-// use std::time::Duration;
 
 pub fn run(){
-    println!("This is a demo function");
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
-            let resp = reqwest::blocking::get("https://httpbin.org/ip");
-            println!("{:#?}", resp);
-        }
+    let mut mqttoptions = MqttOptions::new("rumqtt-sync", "mqtt.eclipseprojects.io", 1883);
+    mqttoptions.set_keep_alive(Duration::from_secs(5));
+    
+    let (mut client, mut connection) = Client::new(mqttoptions, 10);
+    client.subscribe("hello/rumqtt", QoS::AtMostOnce).unwrap();
+    thread::spawn(move || for i in 0..10 {
+       client.publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize]).unwrap();
+       thread::sleep(Duration::from_millis(1000));
     });
-
-    for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
+    
+    // Iterate to poll the eventloop for connection progress
+    for (i, notification) in connection.iter().enumerate() {
+        println!("Notification = {:?}", notification);
     }
-
-    handle.join().unwrap();
 }
