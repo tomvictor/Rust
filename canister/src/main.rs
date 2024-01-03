9453f8a6815f4c00d7b523a488dea92a469119f7
Tom Victor <vjtomvictor@icloud.com>
2024-01-03 08:57:46 +0100

    call to blockchain successful

--- canister/src/main.rs
+++ canister/src/main.rs
@@ -1,8 +1,14 @@
-
 use ic_agent::{Agent, identity::AnonymousIdentity};
+use ic_agent::agent::QueryBuilder;
 use candid::{Principal, Encode, Decode, CandidType, Nat};
 use serde::Deserialize;
 
+
+
+#[derive(CandidType, Deserialize)]
+struct CanisterResult {
+    canister_id: Principal,
+}
 
 async fn connect_to_canister() {
     let agent = Agent::builder()
@@ -14,21 +20,27 @@ async fn connect_to_canister() {
     agent.fetch_root_key().await.unwrap();
     println!("agent created");
 
-    let canister_id = Principal::from_text("bkyz2-fmaaa-aaaaa-qaaaq-cai").unwrap();
-    println!("Canister id: {}", canister_id);
+    let canister_id_principal = Principal::from_text("bkyz2-fmaaa-aaaaa-qaaaq-cai").unwrap();
+    println!("Canister id: {}", canister_id_principal);
 
+    let query_builder = QueryBuilder::new(&agent, canister_id_principal, String::from("hello"));
 
-    let response = agent.query(&canister_id, "hello").call().await;
-    // let response = agent.status();
+    // let bytes = Encode!(&[(42, "arg1")], &(42u32, "arg2")).expect("Panic");
+    let input = Encode!().expect("Panic");
+
+    let response = query_builder
+        .with_arg(input)
+        .call()
+        .await;
 
     match response {
         Ok(resp) => {
             println!("response ok");
-            // println!("response ok: {}", resp.to_string());
+            let result = Decode!(resp.as_slice(), String).expect("Panic: Failed to decode!");
+            println!("{}", result)
         }
         Err(error) => {
-            println!("error occurred: {}", error);
-            // error.to_string()
+            println!("error occurred: {}", error.to_string());
         }
     }
 }
